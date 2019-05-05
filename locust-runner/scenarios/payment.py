b661578462785f9cb2b8da811a46c35465eaf9a7
ze-lin <836149932@qq.com>
2019-05-05 10:45:23 +0800

    payment

--- locust-runner/scenarios/payment.py
+++ locust-runner/scenarios/payment.py
@@ -2,7 +2,7 @@ from locust import HttpLocust, TaskSet, task, seq_task
 import random
 
 class UserBehavior(TaskSet):
-    items = ["03fef6ac-1896-4ce8-bd69-b798f85c6e0b", "3395a43e-2d88-40de-b95f-e00e1502085b", "510a0d7e-8e83-4193-b483-e27e09ddc34d", "808a2de1-1aaa-4c25-a9b9-6612e8f29a38", "819e1fbf-8b7e-4f6d-811f-693534916a8b", "837ab141-399e-4c1f-9abc-bace40296bac", "a0a4f044-b040-410d-8ead-4de0446aec7e", "d3588630-ad8e-49df-bbd7-3167f7efb246",
+    items = ["3395a43e-2d88-40de-b95f-e00e1502085b", "510a0d7e-8e83-4193-b483-e27e09ddc34d", "808a2de1-1aaa-4c25-a9b9-6612e8f29a38", "819e1fbf-8b7e-4f6d-811f-693534916a8b", "837ab141-399e-4c1f-9abc-bace40296bac", "a0a4f044-b040-410d-8ead-4de0446aec7e", "d3588630-ad8e-49df-bbd7-3167f7efb246",
              "zzz4f044-b040-410d-8ead-4de0446aec7e"]
              
     def on_start(self):
@@ -41,6 +41,7 @@ class UserBehavior(TaskSet):
     @seq_task(4)
     def payment(self):
         self.client.post("/orders")
+        self.client.delete("/cart")
 	
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
